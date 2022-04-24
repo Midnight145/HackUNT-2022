1270ed5da14449ba706a2f163856f0566b2d3273
Midnight <rubikscubeboy27@gmail.com>
2022-04-23 21:11:42 -0500

    whitespace

--- server/auth.py
+++ server/auth.py
@@ -11,5 +11,6 @@ def hash_new_password(password):
 def verify_password(password, hashed_password, salt):
     password = password.encode('utf-8')
     # hashed_password = bytes(list(hashed_password))
+    print(password, hashed_password, salt)
     new_hash = hashlib.pbkdf2_hmac('sha256', password, salt, 100000, dklen=128)
     return new_hash == hashed_password

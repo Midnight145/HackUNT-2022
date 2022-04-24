16c98494add10661a657d6aae00e672cca5bdc52
Midnight <rubikscubeboy27@gmail.com>
2022-04-23 21:07:59 -0500

    move away from user/pass based auth

--- client.py
+++ client.py
@@ -3,37 +3,42 @@ import hashlib
 import os
 import sys
 import json
+import shlex
+import uuid
 
 with open("uuid.dat", "w+") as f:
     uuid_ = f.read()
 
 
 def parse_command(x):
-    return x.split(" ")
+    return shlex.split(x)
 
 
-PORT = 31337
+PORT = 31338
 HOST = "127.0.0.1"
-login = False
+register = False
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT))
     while True:
         try:
             data = sock.recv(1024)
-            if login and not uuid_:
+            # if register and not uuid_:
+            if not uuid_:
                 with open("uuid.dat", "w+") as f:
-                    f.write(data.decode())
-                    uuid_ = data.decode()
-                login = False
+                    uuid_ = str(uuid.uuid4())
+                    f.write(str(uuid_))
+                # register = False
             if not data:
                 break
             print(data.decode())
             sendval = input("Enter a message: ")
+            print(sendval)
             ret = parse_command(sendval)
-            if ret[0] == "login":
-                login = True
-            else:
-                sendval = uuid_ + " " + sendval
+            # if ret[0] == "register":
+            #     register = True
+            # else:
+            print(uuid_)
+            sendval = uuid_ + " " + sendval
             sock.send(sendval.encode())
         except KeyboardInterrupt:
             sock.close()

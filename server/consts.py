d7fb4c8f1283a314fb6198a8a9da11e1b4cc0f72
Midnight <rubikscubeboy27@gmail.com>
2022-04-24 02:44:16 -0500

    offload error messages into their own class

---
+++ server/consts.py
@@ -0,0 +1,8 @@
+class Errors:
+    INVALID_PARAMS = "failure::invalid_params"
+    PERMISSION_DENIED = "failure::permission_denied"
+    DUP_SEATS = "failure::dup_seats"
+    SEATS_TAKEN = "failure::seats_taken"
+    INVALID_SEATS = "failure::invalid_seats"
+    NOT_FOUND = "failure::not_found"
+    INVALID_COMMAND = "failure::invalid_command"

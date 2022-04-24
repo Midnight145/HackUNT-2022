415609fbfc8febfece2b8a804f30cabc7076c09a
Midnight <rubikscubeboy27@gmail.com>
2022-04-23 21:08:18 -0500

    add seat struct

--- server/structs.py
+++ server/structs.py
@@ -17,3 +17,28 @@ class SeatReservation:
         self.theater = theater
         self.time = time
         self.seat = seat
+
+
+class Seat:
+    def __init__(self, location=None, row=None, col=None, reserved=False):
+        self.row = row
+        self.col = col
+        self.reserved = reserved
+        self.location = location
+
+    @staticmethod
+    def from_int(seat_int):
+        reserved_mask = 0b10000000
+        reserved = seat_int & reserved_mask
+        location_mask = 0b01111111
+        location = seat_int & location_mask
+        row = location // 10
+        col = location % 10
+        return Seat(location, row, col, reserved)
+
+    # todo: test
+    def to_int(self):
+        reserved_mask = 0b10000000
+        retval = self.reserved & reserved_mask
+        retval += self.location
+        return retval
